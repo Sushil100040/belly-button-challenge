367faf2844352d0d8e8b2fc50b67a7853ee80770
Sushil100040 <Sushilbaskota2013@gmail.com>
2023-05-13 21:54:56 +0800

    a

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+d3.jason(url).then(function (data) {
+    console.log(data);
+});
+
+
+
