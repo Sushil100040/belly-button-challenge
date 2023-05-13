dd769f28757c843a0e4c6e1566e92284759a22b5
Sushil100040 <Sushilbaskota2013@gmail.com>
2023-05-13 22:08:20 +0800

    a

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,23 @@
+//place url in a constant variable
+
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+
+//Fetch the JSON data and console log it
 d3.jason(url).then(function (data) {
     console.log(data);
 });
 
+//function to that builds the bar graph
 
+function updateBarChart(sample) {
+
+// use D3 to retrive all of the data
+    d3.json(url).then((data)=>{
+        let samples = data.samples;
+        let result = samples.filter(sampleObj => sampleObj.id==sample)[
+            
+        ]
+    })
+}
 
